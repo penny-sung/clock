ea616e4e448d8b87520d0256e8ab3ca35e85ca7f
宋沛玲 <50915113@gm.nfu.edu.tw>
2025-01-07 17:58:01 +0800

    Update clockwidget.cpp

--- clockwidget.cpp
+++ clockwidget.cpp
@@ -4,28 +4,25 @@
 #include <QRandomGenerator>
 #include <QFontMetrics>
 
-// Constructor
 ClockWidget::ClockWidget(QWidget *parent)
     : QWidget(parent), clockStyle(ANALOG_CLOCK) {  // Initialize clockStyle
     setFixedSize(400, 400);
 
-    // Timer for updates
+    // update
     QTimer *timer = new QTimer(this);
     connect(timer, &QTimer::timeout, this, QOverload<>::of(&ClockWidget::update));
     timer->start(1000);  // Update every second
 }
 
-// Set the clock style
+//clock style
 void ClockWidget::setClockStyle(ClockStyle style) {
     clockStyle = style;
     update();
 }
 
-// Paint event
 void ClockWidget::paintEvent(QPaintEvent *) {
     QPainter painter(this);
 
-    // Retrieve the current time
     QTime time = QTime::currentTime();
 
     switch (clockStyle) {
@@ -47,13 +44,11 @@ void ClockWidget::paintEvent(QPaintEvent *) {
     }
 }
 
-// Draw analog clock
 void ClockWidget::drawAnalogClock(QPainter &painter) {
     painter.setRenderHint(QPainter::Antialiasing);
     painter.translate(width() / 2, height() / 2);
     int radius = qMin(width(), height()) / 2 - 10;
 
-    // Draw clock face
     painter.setPen(Qt::NoPen);
     painter.setBrush(Qt::white);
     painter.drawEllipse(QPoint(0, 0), radius, radius);
@@ -61,19 +56,19 @@ void ClockWidget::drawAnalogClock(QPainter &painter) {
     QTime time = QTime::currentTime();
     painter.setPen(Qt::black);
 
-    // Hour hand
+    // Hour
     painter.save();
     painter.rotate(30.0 * (time.hour() % 12) + time.minute() / 2.0);
     painter.drawRect(-5, -radius / 3, 10, radius / 3);
     painter.restore();
 
-    // Minute hand
+    // Minute
     painter.save();
     painter.rotate(6.0 * time.minute() + time.second() / 10.0);
     painter.drawRect(-3, -radius / 2, 6, radius / 2);
     painter.restore();
 
-    // Second hand
+    // Second
     painter.setPen(Qt::red);
     painter.save();
     painter.rotate(6.0 * time.second());
@@ -81,7 +76,6 @@ void ClockWidget::drawAnalogClock(QPainter &painter) {
     painter.restore();
 }
 
-// Draw digital clock
 void ClockWidget::drawDigitalClock(QPainter &painter, const QString &timeString) {
     painter.setRenderHint(QPainter::TextAntialiasing);
     QFont font("Arial", 24, QFont::Bold);
@@ -94,7 +88,7 @@ void ClockWidget::drawDigitalClock(QPainter &painter, const QString &timeString)
     painter.drawText((width() - textWidth) / 2, height() / 2, timeString);
 }
 
-// Draw simulated CPU/Memory usage
+
 void ClockWidget::drawCpuMemoryUsage(QPainter &painter) {
     QString usage = getCpuMemoryUsage();
 
@@ -105,7 +99,6 @@ void ClockWidget::drawCpuMemoryUsage(QPainter &painter) {
     painter.drawText(10, height() - 20, usage);
 }
 
-// Simulated CPU/Memory usage function
 QString ClockWidget::getCpuMemoryUsage() {
     int cpuUsage = QRandomGenerator::global()->bounded(1, 100);
     int memoryUsage = QRandomGenerator::global()->bounded(1, 100);
